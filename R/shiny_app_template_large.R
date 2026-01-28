70ad498ff495ae3d3099905e6b32a973deb5c946
jasonm03 <jason.mcglasson2@phs.scot>
2026-01-28 15:00:55 +0000

    Create 2 separate function scripts as these will differ in future.

--- R/shiny_app_template_large.R
+++ R/shiny_app_template_large.R
@@ -1,31 +1,3 @@
-shiny_app_template_basic <- function(
-  app_name = "WRITE APP NAME HERE",
-  author = get_name()
-) {
-  author <- paste("# Original author(s):", author)
-  orig_date <- paste("# Original date:", Sys.Date())
-
-  r_code <- c(
-    "##########################################################",
-    paste0("# ", app_name),
-    author,
-    orig_date,
-    run_on(),
-    "# Description of content",
-    "##########################################################",
-    "",
-    "",
-    "# Get packages",
-    'source("setup.R")',
-    "",
-    ""
-  )
-
-  r_code <- paste(r_code, collapse = "\n")
-
-  return(r_code)
-}
-
 shiny_app_template_large <- function(
     app_name = "WRITE APP NAME HERE",
     author = get_name()
@@ -53,3 +25,5 @@ shiny_app_template_large <- function(
 
   return(r_code)
 }
+
+
